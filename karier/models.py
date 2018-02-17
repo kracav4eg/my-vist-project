fce427849311edc73ff46af761ceef5f652f1b9e
valiev <iamrystam@gmail.com>
2018-02-17 12:25:17 +0000

    Little update never hurts nobody

--- karier/models.py
+++ karier/models.py
@@ -1,30 +1,13 @@
 from django.db import models
 
-"""
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-"""
-class TruckModel(models.Model):
+class TruckModel(models.Model): # Model of the truck model
     name = models.CharField(max_length=50,null=True, blank=True)
     max_weight = models.FloatField()
 
     def __str__(self):
         return self.name
 
-class Truck(models.Model):
+class Truck(models.Model): # Model of truck
     name = models.CharField(max_length=50,null=True, blank=True)
     model = models.ForeignKey('TruckModel')
     curr_weight = models.FloatField()
